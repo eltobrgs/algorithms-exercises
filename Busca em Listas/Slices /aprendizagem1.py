e81f00e16e593273bba0beb1c5358fb65e2e676c
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-04-06 08:46:33 -0300

    Adicionando exercicios de Slices

--- Busca em Listas/Slices /aprendizagem1.py
+++ Busca em Listas/Slices /aprendizagem1.py
@@ -1,11 +1,11 @@
-a= [5, 3, 8, 1, 9, 2, 7, 4, 6, 10, 14, 11, 13, 12, 15]
-print('Sem ordenaçao:', a)
+a = [5, 3, 8, 1, 9, 2, 7, 4, 6, 10, 14, 11, 13, 12, 15]
+print('Sem ordenação:', a)  # Imprime a lista original
 
-for i in range (len(a)-1):
-    for j in range (1+i, len(a)):
-        if a[i]>a[j]:
-            a[i], a[j]= a[j], a[i] 
+for i in range(len(a)-1):  # Loop externo para percorrer a lista
+    for j in range(1+i, len(a)):  # Loop interno para comparar os elementos
+        if a[i] > a[j]:  # Verifica se o elemento atual é maior que o próximo
+            a[i], a[j] = a[j], a[i]  # Troca os elementos de posição se necessário
 
-print('Ordernado com bubble sort:', a)
+print('Ordenado com bubble sort:', a)  # Imprime a lista ordenada usando o Bubble Sort
 
-print('Ordenado em ordem de-crescente com slice', a[::-1])
+print('Ordenado em ordem decrescente com slice:', a[::-1])  # Imprime a lista ordenada em ordem decrescente usando o slice
--- Busca em Listas/Slices /aprendizagem2.py
+++ Busca em Listas/Slices /aprendizagem2.py
@@ -1,13 +1,19 @@
 a = [5, 3, 8, 1, 9, 2, 7, 4, 6, 10, 14, 11, 13, 12, 15]
 
+# Inicializa as variáveis 'menor' e 'maior' com o primeiro elemento da lista 'a'
 menor = a[0]
 maior = a[0]
 
+# Itera sobre cada elemento 'i' na lista 'a'
 for i in a:
+    # Verifica se o elemento atual 'i' é menor que o menor elemento atual 'menor'
     if i < menor:
-        menor = i
-    if i > maior: # neste caso pode ser usado um else em vez de if
-        maior = i
+        menor = i  # Atualiza o valor de 'menor' se 'i' for menor
 
+    # Verifica se o elemento atual 'i' é maior que o maior elemento atual 'maior'
+    if i > maior:
+        maior = i  # Atualiza o valor de 'maior' se 'i' for maior
+
+# Imprime os elementos menor e maior
 print('Menor elemento:', menor)
 print('Maior elemento:', maior)

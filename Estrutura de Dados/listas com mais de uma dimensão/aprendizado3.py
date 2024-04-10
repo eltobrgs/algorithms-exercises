2f47b8cd172951eb9a2ae4865a5241a89c078847
eltobrgs <145704868+eltobrgs@users.noreply.github.com>
2024-04-10 09:32:16 -0300

    Delete Estrutura de Dados/listas com mais de uma dimensão directory

--- Estrutura de Dados/listas com mais de uma dimensão/aprendizado3.py
+++
@@ -1,9 +0,0 @@
-temps=[[0.0 for h in range(24)] for d in range(31)]
-print (temps)
-#the matrix is is magically update here
-max_temp=-100.0
-for day in temps:
-    for temp in day:
-        if temp>max_temp:
-            max_temp=temp
-print("Max temperature was:",max_temp)

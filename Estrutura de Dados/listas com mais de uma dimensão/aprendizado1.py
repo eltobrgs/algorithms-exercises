fb2b36b0c02053ad3de76b24514b1e74e05db220
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-04-10 07:59:22 -0300

    Organizando pastas

---
+++ Estrutura de Dados/listas com mais de uma dimensão/aprendizado1.py
@@ -0,0 +1,15 @@
+'''Listas de duas ou mais dimensões em Python são semelhantes a matrizes na matemática.'''
+
+'''Uma lista de duas dimensões é como uma tabela de números. 
+Por exemplo, [[1, 2, 3], [4, 5, 6], [7, 8, 9]] é uma lista de duas dimensões com 3 linhas e 3 colunas. 
+Cada elemento da lista principal é uma lista.'''
+
+# Criando uma lista de duas dimensões
+lista = [[1, 2, 3], 
+         [4, 5, 6], 
+         [7, 8, 9]]
+
+# Acessando elementos
+primeiro_elemento = lista[0][0]  # Retorna 1
+segundo_elemento = lista[0][1]  # Retorna 2
+terceiro_elemento = lista[1][0]  # Retorna 4

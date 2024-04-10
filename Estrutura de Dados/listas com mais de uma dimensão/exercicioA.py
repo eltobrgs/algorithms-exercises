b80965a8f35fc522f9efd59ae02922102efaf5f2
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-04-10 09:34:51 -0300

    Atualização de exercicios e organização de paginas

---
+++ Estrutura de Dados/listas com mais de uma dimensão/exercicioA.py
@@ -0,0 +1,32 @@
+import random
+a = [[random.randint(1, 10) for i in range(5)] for j in range(3)]
+b = [[random.randint(1, 10) for i in range(5)] for j in range(3)]
+c=[]
+
+
+for i in range(3):
+  for j in range(5):    
+    c.append(a[i][j]+b[i][j])
+
+print(a)
+print(b)
+print(c)
+
+''''
+#jeito do professor 
+import random
+a=[]
+b=[]
+c=[]
+for i in range(5):
+  a.append([])
+  b.append([])
+  c.append([])
+  for j in range(3):
+    a[i].append(random.randint(0,10))
+    b[i].append(random.randint(0,10))
+    c[i].append(a[i][j]+b[i][j])
+print(a)
+print(b)
+print(c)  
+'''

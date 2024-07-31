db1d9b24a2debc38b27e5d42279768e6bda05d24
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-07-31 08:32:03 -0300

    melhoras

--- Exercicios referentes a primeira avaliação/exercicio01.py
+++ Exercicios referentes a primeira avaliação/exercicio01.py
@@ -2,12 +2,13 @@
 # Faça um programa que calcule o fatorial de todos os numeros impares 
 # dentre os 10 primeiros da sequência de Fibonacci. 
 
-fibonacci=(1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
-for i in fibonacci:
-    if i%2!=0:
-        fatorial=1
-        for j in range(1,i+1):
-            fatorial*=j
+fibonacci = [1,1]
+for i in range(2, 10):
+    fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+    print(fibonacci[i])
+for num in fibonacci:
+    if num % 2 != 0:
+        fatorial = 1
+        for j in range(1, num+1):
+            fatorial *= j
         print(fatorial)
-
-        

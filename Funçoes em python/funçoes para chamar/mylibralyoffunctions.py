eb547e31459f51016c51721e5f82c565988f30da
Elto Borges <eltobrgs@MacBook-Air-de-Elto.local>
2024-08-02 11:43:39 -0300

    atualizando exercicios e minha libraly

--- Funçoes em python/funçoes para chamar/mylibralyoffunctions.py
+++ Funçoes em python/funçoes para chamar/mylibralyoffunctions.py
@@ -116,4 +116,39 @@ def calcular_media(numeros):
     media=soma/calcular_tamanho_lista(numeros)
     return media
 
+def cpfvalido(cpf):
+    # Verifica se o CPF tem 11 dígitos
+    if len(cpf) != 11 or not cpf.isdigit():
+        return "CPF inválido."
+    else:
+        # Converte o CPF em uma lista de inteiros
+        cpf = [int(digit) for digit in cpf]
+
+        # Verifica se todos os caracteres são dígitos
+        is_valid = True
+        for digit in cpf:
+            if digit < 0 or digit > 9:
+                is_valid = False
+                break
+
+        if is_valid:
+            # Calcula o primeiro dígito verificador
+            soma1 = 0
+            for i in range(9):
+                soma1 += cpf[i] * (10 - i)
+            digito1 = (soma1 * 10 % 11) % 10
+
+            # Calcula o segundo dígito verificador
+            soma2 = 0
+            for i in range(10):
+                soma2 += cpf[i] * (11 - i)
+            digito2 = (soma2 * 10 % 11) % 10
+
+            # Verifica se os dígitos calculados são iguais aos fornecidos
+            if digito1 == cpf[9] and digito2 == cpf[10]:
+                return "CPF válido."
+            else:
+                return "CPF inválido."
+        else:
+            return "CPF inválido."
 
